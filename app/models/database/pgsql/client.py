6fd7c399b1ce412a74173f68bef47099575ebc6d
Romelito Cadano <mel.cdnz@gmail.com>
2025-03-14 21:24:11 +0800

    Add docs string

--- app/models/database/pgsql/client.py
+++ app/models/database/pgsql/client.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 
 class Client(CoreModel):
+    """Client Model"""
+    
     __tablename__ = "Clients"
 
     Id: Mapped[int] = mapped_column(primary_key=True)
